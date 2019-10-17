b927a5707fbb5934b16152b5cbf58a5b46d7438b
tparris4 <45776810+tparris4@users.noreply.github.com>
2019-10-17 16:53:14 -0400

    Update sqlinsert1.sql

--- sqlinsert1.sql
+++ sqlinsert1.sql
@@ -80,3 +80,46 @@ INSERT INTO UndergradFullTime VALUES ('12.0', 'Met', '2000');
 #Undergraduate Part Time
 
 #Graduate Part Time
+
+
+-- -----------------------------------------------------
+-- Data for table `Register_System`.`User`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `Register_System`;
+INSERT INTO `Register_System`.`User` (`User_ID`, `Email_Address`, `Password`, `First_Name`, `Last_Name`, `Phone_Number`, `Home_Address`) VALUES (10001, 'JDoe123@musc.com', 'DoeJ123', 'John', 'Doe', '(305)886-2675', '345 Washington Avenue');
+INSERT INTO `Register_System`.`User` (`User_ID`, `Email_Address`, `Password`, `First_Name`, `Last_Name`, `Phone_Number`, `Home_Address`) VALUES (20001, 'JohnB234@musc.com', 'BobJohn111', 'Bob', 'John', '(541)505-7964', '123 Delaware Avenue');
+INSERT INTO `Register_System`.`User` (`User_ID`, `Email_Address`, `Password`, `First_Name`, `Last_Name`, `Phone_Number`, `Home_Address`) VALUES (30001, 'JDell1@musc.com', 'DellJ890', 'Jessie', 'Dell', '(323)574-0117', '111 Florida Road');
+INSERT INTO `Register_System`.`User` (`User_ID`, `Email_Address`, `Password`, `First_Name`, `Last_Name`, `Phone_Number`, `Home_Address`) VALUES (40001, 'JWind123@musc.com', 'WindJ12', 'James', 'Wind', '(665)454-1916', '222 Maine Avenue');
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `Register_System`.`Admin`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `Register_System`;
+INSERT INTO `Register_System`.`Admin` (`Admin_ID`, `A_EmailAdd`, `A_Password`, `A_First`, `A_LastName`, `A_PhoneNum`, `A_HomeAdd`, `A_DepartID`, `OfficeNum`, `A_Title`) VALUES (10001, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `Register_System`.`Student`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `Register_System`;
+INSERT INTO `Register_System`.`Student` (`Stud_ID`, `S_First`, `S_Last`, `S_Email`, `S_PhoneNum`, `S_HomeAdd`, `S_Password`, `S_Type`, `S_AcaStand`) VALUES (20001, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT, 'Undergraduate', 'Good');
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `Register_System`.`Researcher`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `Register_System`;
+INSERT INTO `Register_System`.`Researcher` (`Research_ID`, `R_First`, `R_Last`, `R_Email`, `R_PhoneNum`, `R_HomeAdd`, `R_Password`) VALUES (40001, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT);
+
+COMMIT;
