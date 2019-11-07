7f9db7562e1ff7c1c1da3653ec69aa28b5cc15c5
tparris4 <45776810+tparris4@users.noreply.github.com>
2019-11-07 15:30:46 -0500

    Update department.sql

--- department.sql
+++ department.sql
@@ -1,23 +1,3 @@
-#NAME?
--- version 4.9.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
-"-- Generation Time: Nov 07, 2019 at 09:08 PM"
--- Server version: 10.4.8-MariaDB
--- PHP Version: 7.3.10
-
-"SET SQL_MODE = ""NO_AUTO_VALUE_ON_ZERO"";"
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-"SET time_zone = ""+00:00"";"
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 
 --
 -- Dumping data for table `department`
@@ -26,11 +6,3 @@ START TRANSACTION;
 "INSERT INTO `department` (`Department_ID`, `D_Name`, `D_Email`, `D_Location`, `D_Phone`, `D_Secretary`, `Professor_ChairID`) VALUES"
 
 "(130, 'Test', 'Test', 'Test', 'Test', 'Test', 0);"
---
-#NAME?
---
-
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
