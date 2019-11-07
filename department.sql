ff789565c8d5451cc7841c7208c8e617d65f26dd
tparris4 <45776810+tparris4@users.noreply.github.com>
2019-11-07 15:31:53 -0500

    Update department.sql

--- department.sql
+++ department.sql
@@ -1,8 +1,2 @@
-
---
--- Dumping data for table `department`
---
-
 "INSERT INTO `department` (`Department_ID`, `D_Name`, `D_Email`, `D_Location`, `D_Phone`, `D_Secretary`, `Professor_ChairID`) VALUES"
-
 "(130, 'Test', 'Test', 'Test', 'Test', 'Test', 0);"
