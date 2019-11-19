313e9e348fb98cd8690c1c7fcc5d944873132c32
tparris4 <45776810+tparris4@users.noreply.github.com>
2019-11-18 20:21:04 -0500

    Add files via upload

---
+++ CrsCatalogBusiness.php
@@ -0,0 +1,77 @@
+<?php
+ $dbhost = 'localhost';
+   $dbuser = 'root';
+   $dbpass = '';
+   $dbname = 'register_system';
+   $conn = mysqli_connect($dbhost, $dbuser, $dbpass,$dbname);
+   
+   if(! $conn ) {
+      die('Could not connect: ' . mysqli_error());
+   }
+   echo 'Connected successfully</br>';
+   session_start();
+   ?>
+<html>
+<head>
+<?php 
+require "header2.php";
+?>
+    <meta charset = "utf-8">
+<link rel="stylesheet" type="text/css" href="style.css">
+</style>
+<body>
+
+
+<h2>Biology Course Catalog</h2>
+
+ <?php 
+           // $conn = mysqli_connect($dbhost, $dbuser, $dbpass,$dbname);
+            $sql = "SELECT DISTINCT c.C_Name, c.C_Description, c.C_Code, c.C_CreditAmt FROM course AS c 
+                JOIN prerequisites1 AS p WHERE c.CDeptID = '126'";
+            if ($result = mysqli_query($conn, $sql)){
+                if(mysqli_num_rows($result) > 0){
+            
+                    echo "<table>"; 
+                    echo"<th>Course_Name</th>";            
+                    echo"<th>Course_Description</th>";
+                    echo"<th>Course_Code</th>";
+                    echo"<th>Course_Credit Amount</th>";
+                    echo"<th>Prerequisites</th>";
+                    echo"</th>";
+                    
+                   
+
+                   while($row = mysqli_fetch_array($result)){
+                    echo "<tr>";
+                    echo"<td>" . $row['C_Name'] . "</td>";
+                    echo"<td>" . $row['C_Description'] . "</td>";
+                    echo"<td>" . $row['C_Code'] . "</td>";
+                    echo"<td>" . $row['C_CreditAmt'] . "</td>";
+                    /*if(prerequisites1.Prerequ_ID < prerequisites1.P_CourseID && prerequisites1.P_courseID == course.Course_ID){
+                    echo"<td>" . $row['C_Name'] . "</td>";}
+                    else{
+                        echo"<th>None</th>";
+                    } */
+                    echo"</tr>";
+                    }
+                echo "</table>";
+      mysqli_free_result($result);
+    } else {
+      echo "Not found";
+    }
+   } else{
+    echo "Error: could not execute $sql. " . mysqli_error($conn);
+   }
+   mysqli_close($conn);
+?>
+
+
+
+</body>
+
+</html>
+
+<br><br><br><br><br><br>
+<?php 
+require 'footer.php';
+?>
