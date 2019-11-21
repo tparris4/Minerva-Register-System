1aa5da45f717175e7c4b0e5d458d202874308089
tparris4 <45776810+tparris4@users.noreply.github.com>
2019-11-21 15:55:43 -0500

    Add files via upload

--- config.php
+++ config.php
@@ -4,23 +4,18 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
+
         <?php
-        define('DB_SERVER', 'localhost');
-        define('DB_USERNAME', 'root');
-        define('DB_PASSWORD', 'mysqlloz123!');
-        define('DB_NAME', 'demo');
-        
-        $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-        
-        if($link === false){
-            die("Error: Could not connect. " . mysqli_connect_error());
-        }
+ $dbhost = 'localhost';
+   $dbuser = 'root';
+   $dbpass = '';
+   $dbname = 'register_system';
+   $conn = mysqli_connect($dbhost, $dbuser, $dbpass,$dbname);
+   
+   if(! $conn ) {
+      die('Could not connect: ' . mysqli_error());
+   }
+   echo 'Connected successfully</br>';
+   
         ?>
-    </body>
-</html>
+  
