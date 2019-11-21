943e26824a382456cafa4f50261493364c9066f3
tparris4 <45776810+tparris4@users.noreply.github.com>
2019-11-21 15:54:58 -0500

    Add files via upload

---
+++ Index1.php
@@ -0,0 +1,43 @@
+<?php
+include "header3.php";
+?>
+
+        <div id="container">
+          <div id="main">
+              <section class="wrapper2">
+                <div class="home-banner"></div>
+                <div align="center"><h1>Welcome to Minerva University</h1></div>
+              </section>
+              <section class="wrapper">
+                <p>A University where we strive to uplift our
+                  youths to reach their highest potential
+                  on our campus we value personal growth.
+                </p>
+                <br>
+                <br>
+                <p> We pride ourselves in providing a learning
+                  environment unlike any other
+                  for our students. Where they will learn 
+                  the ropes that will be useful in the field.
+                </p>
+              </section>
+              <section class="wrapper3">
+              </section>
+          </div>
+        </div>
+<br
+    <br>
+<br><br><br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+
+<?php
+include "footer.php";
+?>
+</body>
+</html>
