b6adb83179126c11e7c6c30b9bc5b17953918f44
mcali509 <55856588+mcali509@users.noreply.github.com>
2019-12-03 17:22:48 -0500

    Add files via upload

---
+++ StudentButtons.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <meta charset = "utf-8">
+      <title>Student</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" type="text/css" href="style.css">
+   </head>
+   <body>
+    <header>
+    </header>
+      <div id="Homepagecontainer">
+      <div id="main">
+      <section class="wrapper">
+      <div class="btn-group" style="width:100%">
+      <button style="width:11.11%"><a href="Addcourse.php">Add Course</a></button>
+      <button style="width:11.11%"><a href="DropCourse.php">Drop Course</a></button>
+      <button style="width:11.11%"><a href="ClassSearch.php">Look up course</a></button>
+      <button style="width:11.11%"><a href="LookUpTranscript.php">View Transcript</a></button>
+      <button style="width:11.11%"><a href="LookUpHold.php">View Hold</a></button>
+      <button style="width:11.11%"><a href="ViewGrades.php">View Grades</a></button>
+      <button style="width:11.11%"><a href="ViewCatalog.php">View Catalog</a></button>
+      <button style="width:11.11%"><a href="ChangeMajor.php">Declare Major</a></button>
+      <button style="width:11.11%"><a href="DegreeAudit.php">Degree Audit</a></button>
+      </div>
+      </section>
+      </div>
+      </div>
+
+    <footer>
+    </footer>
+    </body>
+</html>
